cbcc7a9fef4250676de50420521b103ebffd761b
VividHydra28 <thatomahao28@gmail.com>
2026-08-18 22:41:36 +0200

    dashboard

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>Job Application Tracker</h1>
+      <p>Track your job applications, interviews and offers.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
